538de75e491a2804dcae54495b0d3135fbeec3b6
Carrie Kraft <carriekarft@gmail.com>
2022-12-17 19:23:07 -0800

    commit tree 2713e851bc313da4f06a26cbe005bb440ba3bbbf

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+    })
+    .then(res => res.json())
+    .then(data => {
+        appending(data.id)
+        console.log(data.id)
+    })
+    .catch(err => {
+        errMessage(err.message)
+        alert('darn!')
+    })
+};
+
+function appending(idValue) {
+    const p = document.createElement('p');
+    p.innerHTML = idValue;
+    document.body.appendChild(p);
+}
+
+function errMessage(errValue) {
+    const p = document.createElement('p');
+    p.innerHTML = errValue;
+    document.body.appendChild(p);
+}
